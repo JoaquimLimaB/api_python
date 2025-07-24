08d61cb870c231b296fbd317798de591590442a7
unknown <joaquim.lima.2023@gmail.com>
2025-07-24 08:35:56 -0300

    nova atualizacao

--- actors/urls.py
+++ actors/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('actors/', views.ActorCreateListView.as_view(), name='actor-create-list'),
-    path('actors/<int:pk>', views.ActorRetriveUpdateDestroyView.as_view(),
+    path('actors/<int:pk>/', views.ActorRetriveUpdateDestroyView.as_view(),
          name='actor-detail-view'),
 ]
